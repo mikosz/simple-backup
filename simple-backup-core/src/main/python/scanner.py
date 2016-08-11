006fdd08e67f0d1b4afcb755010782ee88444b12
Mikolaj Radwan <mikoszrrr@gmail.com>
2016-08-11 14:43:51 +0200

    Finds moved files

--- simple-backup-core/src/main/python/scanner.py
+++ simple-backup-core/src/main/python/scanner.py
@@ -16,27 +16,29 @@ class ScanResult:
 
     class FileEntry:
 
-        def __init__(self, backup_id, local_path, update_time = None, digest = None):
+        def __init__(self, backup_id, local_path, old_local_path = None, update_time = None, digest = None):
             self.backup_id = backup_id
             self.local_path = local_path
+            self.old_local_path = old_local_path
             self.update_time = int(update_time)
             self.digest = digest
 
         def __lt__(self, other):
-            return (self.backup_id, self.local_path, self.update_time, self.digest) < (other.backup_id, other.local_path, other.update_time, other.digest)
+            return (self.backup_id, self.local_path, self.old_local_path, self.update_time, self.digest) < (other.backup_id, other.local_path, self.old_local_path, other.update_time, other.digest)
 
         def __eq__(self, other):
-            return (self.backup_id, self.local_path, self.update_time, self.digest) == (other.backup_id, other.local_path, other.update_time, other.digest)
+            return (self.backup_id, self.local_path, self.old_local_path, self.update_time, self.digest) == (other.backup_id, other.local_path, self.old_local_path, other.update_time, other.digest)
 
         def __hash__(self):
-            return hash((self.backup_id, self.local_path, self.update_time, self.digest))
+            return hash((self.backup_id, self.local_path, self.old_local_path, self.update_time, self.digest))
 
         def __str__(self):
-            return str({ 'backup_id': self.backup_id, 'local_path': self.local_path, 'update_time': self.update_time, 'digest': self.digest })
+            return str({ 'backup_id': self.backup_id, 'local_path': self.local_path, 'old_local_path': self.old_local_path, 'update_time': self.update_time, 'digest': self.digest })
 
     def __init__(self):
         self.added = []
         self.modified = []
+        self.moved = []
         self.removed = []
 
 def scan(localhost, backup_name, db):
@@ -45,6 +47,8 @@ def scan(localhost, backup_name, db):
     backup_info = db.get_backup_info(localhost, backup_name)
     backup_id = backup_info['id']
     backup_root = backup_info['local_root_path']
+
+    added_files_by_digest = {}
 
     logging.info('Scanning directory backup %s at %s', backup_name, backup_root)
 
@@ -55,13 +59,15 @@ def scan(localhost, backup_name, db):
 
             local_file_entry = db.get_local_file(backup_id, local_path=local_path)
 
+            # TODO: check update time and changes in size for quick modification check
             update_time = int(os.path.getmtime(absolute_path))
 
             if local_file_entry == None:
                 file_digest = digest(absolute_path)
-                entry = ScanResult.FileEntry(backup_id, local_path, update_time, file_digest)
+                entry = ScanResult.FileEntry(backup_id, local_path, update_time=update_time, digest=file_digest)
                 result.added.append(entry)
-                logging.info('-- found new file: %s', vars(entry))
+                added_files_by_digest[file_digest] = entry
+                logging.debug('-- found new file: %s', vars(entry))
             else:
                 backup_file_id = local_file_entry['backup_file_id']
                 backup_file = db.get_backup_file(backup_file_id)
@@ -72,9 +78,9 @@ def scan(localhost, backup_name, db):
                 if update_time > backup_file_update_time:
                     file_digest = digest(absolute_path)
                     if file_digest != backup_file_digest:
-                        entry = ScanResult.FileEntry(backup_id, local_path, update_time, file_digest)
+                        entry = ScanResult.FileEntry(backup_id, local_path, update_time=update_time, digest=file_digest)
                         result.modified.append(entry)
-                        logging.info('-- found modified file: %s', vars(entry))
+                        logging.debug('-- found modified file: %s', vars(entry))
 
     backup_files = db.get_backup_files(backup_id)
     for backup_file in backup_files:
@@ -86,8 +92,18 @@ def scan(localhost, backup_name, db):
         local_path = local_file['local_path']
 
         if not os.path.exists(os.path.join(backup_root, local_path)):
-            entry = ScanResult.FileEntry(backup_id, local_path, backup_file_update_time, backup_file_digest)
-            result.removed.append(entry)
-            logging.info('-- removed file: %s', vars(entry))
+            moved = False
+            for added_file in result.added:
+                if added_file.digest == backup_file_digest:
+                    result.added.remove(added_file)
+                    moved_file = added_file
+                    moved_file.old_local_path = local_path
+                    result.moved.append(added_file)
+                    moved = True
+                    logging.debug('-- found moved file: %s', vars(moved_file))
+            if not moved:
+                entry = ScanResult.FileEntry(backup_id, local_path, update_time=backup_file_update_time, digest=backup_file_digest)
+                result.removed.append(entry)
+                logging.debug('-- removed file: %s', vars(entry))
 
     return result
--- simple-backup-core/src/test/python/scanner_test.py
+++ simple-backup-core/src/test/python/scanner_test.py
@@ -9,7 +9,7 @@ import logging
 class Test_scanner_test(unittest.TestCase):
 
     def setUp(self):
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
         self.tmpdir = tempfile.mkdtemp(prefix='scanner_test_')
         return super().setUp()
 
@@ -25,6 +25,7 @@ class Test_scanner_test(unittest.TestCase):
 
         self.assertFalse(changes.added)
         self.assertFalse(changes.modified)
+        self.assertFalse(changes.moved)
         self.assertFalse(changes.removed)
 
     def test_finds_new_files(self):
@@ -36,7 +37,7 @@ class Test_scanner_test(unittest.TestCase):
         loose_file_local_path = 'loose-file.txt'
         loose_file_mtime = 42.123
         loose_file_digest = b'\xe8\x0bP\x17\t\x89P\xfcX\xaa\xd8<\x8c\x14\x97\x8e'
-        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, loose_file_mtime, loose_file_digest)
+        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, update_time=loose_file_mtime, digest=loose_file_digest)
 
         loose_file_path = '%s/%s' % (backup_dir, loose_file_local_path)
         with open(loose_file_path, 'w') as loose:
@@ -51,7 +52,7 @@ class Test_scanner_test(unittest.TestCase):
         file_in_subdir_path = '%s/%s' % (backup_dir, file_in_subdir_local_path)
         file_in_subdir_mtime = 84.246
         file_in_subdir_digest = b'&\xe1b\xd0\xb5paA\xbd\xb9T\x90\n\xeb\xe8\x04'
-        file_in_subdir_entry = scanner.ScanResult.FileEntry(backup_id, file_in_subdir_local_path, file_in_subdir_mtime, file_in_subdir_digest)
+        file_in_subdir_entry = scanner.ScanResult.FileEntry(backup_id, file_in_subdir_local_path, update_time=file_in_subdir_mtime, digest=file_in_subdir_digest)
 
         with open(file_in_subdir_path, 'w') as file_in_subdir:
             file_in_subdir.write('ghijkl')
@@ -61,6 +62,7 @@ class Test_scanner_test(unittest.TestCase):
 
         self.assertCountEqual(changes.added, [ loose_file_entry, file_in_subdir_entry ])
         self.assertCountEqual(changes.modified, [])
+        self.assertCountEqual(changes.moved, [])
         self.assertCountEqual(changes.removed, [])
 
     def test_finds_modified_files(self):
@@ -73,7 +75,7 @@ class Test_scanner_test(unittest.TestCase):
         loose_file_mtime = 42.123
         loose_file_mtime_updated = loose_file_mtime + 3
         loose_file_digest = b'\xe8\x0bP\x17\t\x89P\xfcX\xaa\xd8<\x8c\x14\x97\x8e'
-        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, loose_file_mtime_updated, loose_file_digest)
+        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, update_time=loose_file_mtime_updated, digest=loose_file_digest)
 
         backup_file_id = db.add_backup_file(backup_id, '', loose_file_mtime, loose_file_digest)
         db.add_local_file(backup_id, loose_file_local_path, backup_file_id)
@@ -93,7 +95,7 @@ class Test_scanner_test(unittest.TestCase):
         file_in_subdir_mtime_updated = 84.246 + 3
         file_in_subdir_digest = b'&\xe1b\xd0\xb5paA\xbd\xb9T\x90\n\xeb\xe8\x04'
         file_in_subdir_digest_updated = b'\xd2\xec\x9b2\xd6tAM\x11\x125>\xa8\x80w\x9b'
-        file_in_subdir_entry = scanner.ScanResult.FileEntry(backup_id, file_in_subdir_local_path, file_in_subdir_mtime_updated, file_in_subdir_digest_updated)
+        file_in_subdir_entry = scanner.ScanResult.FileEntry(backup_id, file_in_subdir_local_path, update_time=file_in_subdir_mtime_updated, digest=file_in_subdir_digest_updated)
 
         backup_file_id = db.add_backup_file(backup_id, '', file_in_subdir_mtime, file_in_subdir_digest)
         db.add_local_file(backup_id, file_in_subdir_local_path, backup_file_id)
@@ -106,9 +108,10 @@ class Test_scanner_test(unittest.TestCase):
 
         self.assertCountEqual(changes.added, [])
         self.assertCountEqual(changes.modified, [ file_in_subdir_entry ])
+        self.assertCountEqual(changes.moved, [])
         self.assertCountEqual(changes.removed, [])
 
-    def test_moved_files_are_marked_as_modified(self):
+    def test_finds_moved_files(self):
         db = file_db.FileDB(os.path.join(self.tmpdir, 'test.db'))
         backup_dir = os.path.join(self.tmpdir, 'to_backup')
         os.mkdir(backup_dir)
@@ -118,7 +121,7 @@ class Test_scanner_test(unittest.TestCase):
         loose_file_moved_path = 'loose-file-moved.txt'
         loose_file_mtime = 42.123
         loose_file_digest = b'\xe8\x0bP\x17\t\x89P\xfcX\xaa\xd8<\x8c\x14\x97\x8e'
-        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, loose_file_mtime, loose_file_digest)
+        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_moved_path, old_local_path=loose_file_local_path, update_time=loose_file_mtime, digest=loose_file_digest)
 
         backup_file_id = db.add_backup_file(backup_id, '', loose_file_mtime, loose_file_digest)
         db.add_local_file(backup_id, loose_file_local_path, backup_file_id)
@@ -131,7 +134,8 @@ class Test_scanner_test(unittest.TestCase):
         changes = scanner.scan('local', 'test_backup', db)        
 
         self.assertCountEqual(changes.added, [])
-        self.assertCountEqual(changes.modified, [ loose_file_entry ])
+        self.assertCountEqual(changes.modified, [])
+        self.assertCountEqual(changes.moved, [ loose_file_entry ])
         self.assertCountEqual(changes.removed, [])
 
     def test_finds_removed_files(self):
@@ -143,7 +147,7 @@ class Test_scanner_test(unittest.TestCase):
         loose_file_local_path = 'loose-file.txt'
         loose_file_mtime = 0
         loose_file_digest = b''
-        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, loose_file_mtime, loose_file_digest)
+        loose_file_entry = scanner.ScanResult.FileEntry(backup_id, loose_file_local_path, update_time=loose_file_mtime, digest=loose_file_digest)
 
         backup_file_id = db.add_backup_file(backup_id, '', loose_file_mtime, loose_file_digest)
         db.add_local_file(backup_id, loose_file_local_path, backup_file_id)
@@ -152,6 +156,7 @@ class Test_scanner_test(unittest.TestCase):
 
         self.assertCountEqual(changes.added, [])
         self.assertCountEqual(changes.modified, [])
+        self.assertCountEqual(changes.moved, [])
         self.assertCountEqual(changes.removed, [ loose_file_entry ])
 
 if __name__ == '__main__':
